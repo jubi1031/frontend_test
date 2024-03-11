84a46f73af4452a97c0eb6b03928f0b19527ea5f
jubi <joobe1031@gmail.com>
2024-03-11 15:24:18 +0900

    first commit

---
+++ banner2/banner_slide.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".slide1", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: ".pg1",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".next1",
+    prevEl: ".prev1",
+  },
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+    },
+    1100: {
+      slidesPerView: 4,
+    },
+  },
+});
